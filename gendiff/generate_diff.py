355ca966ba30c57ce3c1470233d4c05ecbf2bb7e
Olga Akukin <olyapa@gmail.com>
2025-02-12 20:37:15 +0100

    linter corrects

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,6 +1,6 @@
 from gendiff.buid_diff import build_diff
-from gendiff.parse import return_dict
 from gendiff.formats.stylish import format_stylish
+from gendiff.parse import return_dict
 
 
 def generate_diff(file_path1, file_path2, format_name='stylish'):
@@ -10,8 +10,8 @@ def generate_diff(file_path1, file_path2, format_name='stylish'):
     if format_name == "stylish":
         return "{\n" + format_stylish(diff) + "\n}"
 
-#print(generate_diff("/Users/olgaakukina/PycharmProjects/python-project-50/tests/test_data/file1.yaml",
-#"/Users/olgaakukina/PycharmProjects/python-project-50/tests/test_data/file2.yaml"))
+# print(generate_diff("/Users/olgaakukina/PycharmProjects/python-project-50/tests/test_data/file1.yaml",
+# "/Users/olgaakukina/PycharmProjects/python-project-50/tests/test_data/file2.yaml"))
 
-print(generate_diff("/Users/olgaakukina/PycharmProjects/python-project-50/tests/test_data/file1.json",
-"/Users/olgaakukina/PycharmProjects/python-project-50/tests/test_data/file2.json", 'stylish'))
+# print(generate_diff("/Users/olgaakukina/PycharmProjects/python-project-50/tests/test_data/file1.json",
+# "/Users/olgaakukina/PycharmProjects/python-project-50/tests/test_data/file2.json", 'stylish'))
