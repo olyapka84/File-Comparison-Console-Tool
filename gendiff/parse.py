3e7f9ba61107fe5eeee8baad9a52d3de660bb2e5
Olga Akukin <olyapa@gmail.com>
2025-03-05 20:28:52 +0100

    added error check to parse.py

--- gendiff/parse.py
+++ gendiff/parse.py
@@ -4,6 +4,9 @@ import yaml
 
 
 def parse_file(file_path):
+    if not (file_path.endswith(".json") or file_path.endswith(".yaml")
+            or file_path.endswith(".yml")):
+        raise ValueError(f"Unsupported file format: {file_path}")
     with open(file_path, 'r') as file:
         if file_path.endswith('.json'):
             return json.load(file)
